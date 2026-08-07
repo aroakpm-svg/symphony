1b2c330e9f90a89480f815fa876134ecfe0574e8
aroakpm-svg <272156804+aroakpm-svg@users.noreply.github.com>
2026-08-07 14:26:05 +0800

    Format ledger namespace assertion

--- elixir/test/symphony_elixir/effect_ledger_migration_test.exs
+++ elixir/test/symphony_elixir/effect_ledger_migration_test.exs
@@ -97,6 +97,7 @@ defmodule SymphonyElixir.EffectLedgerMigrationTest do
     source = File.read!(@effect_ledger)
 
     assert source =~ "select status, native_resource, attempt_id::text"
+
     assert SymphonyElixir.EffectLedger.operation_id("ARO-166", "comment-1") ==
              "ARO-166:comment-1"
     assert source =~ "defp encode_resource(resource) when is_map(resource), do: resource"
