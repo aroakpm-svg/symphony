b005d3f2949491c8b5e011134b82db450fb61c33
aroakpm-svg <aroak.pm@gmail.com>
2026-09-21 11:11:16 +0800

    chore: remove broker CI diagnostics

--- elixir/priv/windows-service/Symphony.WindowsBroker/Program.cs
+++ elixir/priv/windows-service/Symphony.WindowsBroker/Program.cs
@@ -61,20 +61,8 @@ sealed class BrokerWindowsService : ServiceBase
 
     async Task RunAsync()
     {
-        try
-        {
-            await using var server = new BrokerServer(options, factory);
-            await server.RunAsync(stop.Token);
-        }
-        catch (Exception error)
-        {
-            var path = Environment.GetEnvironmentVariable("SYMPHONY_BROKER_SERVICE_DIAGNOSTIC_FILE");
-            if (!string.IsNullOrWhiteSpace(path))
-            {
-                try { File.WriteAllText(path, error.ToString()); } catch { }
-            }
-            throw;
-        }
+        await using var server = new BrokerServer(options, factory);
+        await server.RunAsync(stop.Token);
     }
 
     protected override void Dispose(bool disposing)
